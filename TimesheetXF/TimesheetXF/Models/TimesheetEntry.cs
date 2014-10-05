51e29f0379b01dcda766f349219d6dc2967e3d29
Benjamin Tam <ben@teamtam.com>
2014-10-05 23:00:02 +1100

    Models

---
+++ TimesheetXF/TimesheetXF/Models/TimesheetEntry.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TimesheetXF.Models
+{
+    public class TimesheetEntry
+    {
+        public Guid TimesheetId { get; set; }
+        public DateTime Date { get; set; }
+        public string Customer { get; set; }
+        public string Project { get; set; }
+        public decimal Hours { get; set; }
+        public string Comment { get; set; }
+        public bool SickLeave { get; set; }
+    }
+}
